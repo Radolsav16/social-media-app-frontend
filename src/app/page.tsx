29f7d5e61f948145d2cfc3695cff05cb2e011935
Radoslav Todorov <todorovradoslav18@gmail.com>
2025-09-27 14:50:19 +0300

    add footer and add components to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
 import Sidebar from "@/components/sidebar/Sidebar";
 
 
 export default function Home() {
   return (
-      <><Header /><Sidebar /></>
+      <>home</>
   );
 }
